64c7d2769b0975f4b95a78c81d963e0ff50efd09
Domengo <dominicsengo@gmail.com>
2025-02-08 03:40:21 +0300

    `Update with minor changes`

--- trendai-frontend/utils/api.ts
+++ trendai-frontend/utils/api.ts
@@ -1,16 +1,56 @@
+// import axios from "axios"
+// import 
+
+// const api = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://trendai-backend-lime.vercel.app",
+// })
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token")
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default api
+
 import axios from "axios"
+import Cookies from "js-cookie"
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://trendai-backend-lime.vercel.app",
+  withCredentials: true, // This allows the browser to send cookies with cross-origin requests
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token")
+  const token = Cookies.get("token")
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 401 && !error.config._retry) {
+      error.config._retry = true
+      try {
+        const res = await api.post("/auth/refresh")
+        const { access_token } = res.data
+        Cookies.set("token", access_token, { expires: 7 }) // Set cookie to expire in 7 days
+        error.config.headers.Authorization = `Bearer ${access_token}`
+        return api(error.config)
+      } catch (refreshError) {
+        Cookies.remove("token")
+        window.location.href = "/login"
+        return Promise.reject(refreshError)
+      }
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default api
 
